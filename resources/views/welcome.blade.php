dcb76d4199d9691ecf6b89f4c833b439201e2030
mdenitti <massimodn77@gmail.com>
2026-03-17 13:35:14 +0100

    Refactor layout references in views to ensure consistency and reallocate the main layout file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,13 @@
-@extends('layout')
+@extends('layout.layout')
 
 @section ('title','Home')
+
+@section('hero')
+    <div class="jumbotron">
+        <h1 class="display-4">Welcome to our course management system!</h1>
+        <p class="lead">Here you can manage your courses, instructors, and categories with ease.</p>
+        <hr class="my-4">
+@endsection
 
 @section('content')
     Hallo, wil je naar de cursus toevoegen pagina gaan? Klik dan op de volgende link: <a href="/courseadd">Add a course</a>
